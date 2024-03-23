b1b9e16be1af10b105cf722ff3e876f1820fe71e
Leo <leo.hilsheimer@gmail.com>
2024-03-23 08:48:50 +0100

    make intro logo bigger

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -30,7 +30,9 @@ const Intro = () => {
       )}
       onClick={() => setHide(true)}
     >
-      {site.logo && <Image media={site.logo as Media} />}
+      {site.logo && (
+        <Image media={site.logo as Media} className="max-w-80vw w-[544px]" />
+      )}
       <button className="bg-key-500 hover:bg-key-600 focus:ring-key-500 mt-12 rounded-lg px-4 py-2 text-white transition-colors duration-200 ease-in-out">
         Zur Haupseite
       </button>
