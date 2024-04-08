115ff7df8979d5d30dfcd3370cd8550ea0c97858
Leo <leo.hilsheimer@gmail.com>
2024-04-08 10:14:36 +0200

    cleanup unused database fields

---
+++ migrations/20240408_080641_cleanup_unused_fields.ts
@@ -0,0 +1,13 @@
+import type { MigrateUpArgs, MigrateDownArgs } from "@payloadcms/db-mongodb";
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  await payload.db.collections.pages?.updateMany(
+    {},
+    { $unset: { layout: "", content_html: "" } },
+    { strict: false },
+  );
+}
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+  // we won't bring back the fields
+}
