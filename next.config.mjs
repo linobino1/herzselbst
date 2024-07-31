b73275ca5206ec724e62deb9d031f984eded9e3c
Leo <leo.hilsheimer@gmail.com>
2024-07-31 12:10:58 +0200

    no-index cms pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  headers: async () => {
+    return [
+      {
+        source: "/admin/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withPayload(nextConfig);
